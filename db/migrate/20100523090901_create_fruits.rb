bfd886596f85297f368c0a8b914dc95fa7d5c9ea
Thomas <thomas@icdesign.com.au>
2010-05-23 19:09:12 +1000

    Added Fruit model generator

---
+++ db/migrate/20100523090901_create_fruits.rb
@@ -0,0 +1,14 @@
+class CreateFruits < ActiveRecord::Migration
+  def self.up
+    create_table :fruits do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :fruits
+  end
+end
